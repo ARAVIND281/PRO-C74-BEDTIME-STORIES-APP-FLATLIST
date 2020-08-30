306225f1f5b72f30ca2abdd246f766ea2c8bd381
S.Aravind <63240570+ARAVIND281@users.noreply.github.com>
2020-08-30 16:40:35 +0530

    Add files via upload

--- screens/ReadScreen.js
+++ screens/ReadScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import { Header } from 'react-native-elements';
+import { StyleSheet, Text, View ,FlatList,ScrollView} from 'react-native';
+import {SearchBar,Header} from 'react-native-elements';
 import * as firebase from 'firebase';
 import db from '../config'
 
@@ -9,89 +8,84 @@ import db from '../config'
 
 
 export default class ReadStoryScreen extends React.Component {
-  constructor() {
+  constructor(){
     super();
-    this.state = {
-      allStories: [],
-      data: [],
-      search: ''
+    this.state ={
+      allStories:[],
+      data:[],
+      search : ''
     }
   }
-  componentDidMount() {
-    this.retrieveStories()
-  }
+  
 
   updateSearch = search => {
     this.setState({ search });
   };
 
 
-  retrieveStories = () => {
-    try {
-      var allStories = []
+  stories=()=>{
+    
+      var allStories= []
       var stories = db.collection("stories")
-        .get().then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-
-            allStories.push(doc.data())
-            console.log('this are the stories', allStories)
+        .get().then((querySnapshot)=> {
+          querySnapshot.forEach((doc)=> {              
+              allStories.push(doc.data())
           })
-          this.setState({ allStories })
+          this.setState({allStories})
         })
-    }
-    catch (error) {
-      console.log(error);
-    }
   };
 
-
   SearchFilterFunction(text) {
-    const newData = this.state.allStories.filter((item) => {
+    //passing the inserted text in textinput
+    const newData = this.state.allStories.filter((item)=> {
+      //applying filter for the inserted text in search bar
       const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
       const textData = text.toUpperCase();
       return itemData.indexOf(textData) > -1;
     });
     this.setState({
+      //setting the filtered newData on data
+      //After setting the data it will automatically re-render the view
       data: newData,
       search: text,
     });
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header
-          backgroundColor={'pink'}
-          centerComponent={{
-            text: 'Bed Time Stories',
-            style: { color: 'white', fontSize: 20 }
-          }}
-        />
-        <View styles={{ height: 20, width: '100%' }}>
-          <SearchBar
-            placeholder="Type Here..."
-            onChangeText={text => this.SearchFilterFunction(text)}
-            onClear={text => this.SearchFilterFunction('')}
-            value={this.state.search}
-          />
-        </View>
-
-        <FlatList
-          data={this.state.search === "" ? this.state.allStories : this.state.data}
-          renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
-              <Text>  Title: {item.title}</Text>
-              <Text>  Author : {item.author}</Text>
-            </View>
-          )}
-          keyExtractor={(item, index) => index.toString()}
-        />
-
-
-
-      </View>
-    );
-  }
+    render(){
+      return(
+        <View style ={styles.container}>
+           <Header 
+                backgroundColor = {'pink'}
+                centerComponent = {{
+                    text : 'Bed Time Stories',
+                    style : { color: 'white', fontSize: 20}
+                }}
+            />
+          <View styles ={{height:20,width:'100%'}}>
+              <SearchBar
+              placeholder="Type Here..."
+              onChangeText={text => this.SearchFilterFunction(text)}
+              onClear={text => this.SearchFilterFunction('')}
+              value={this.state.search}
+            />
+          </View>
+          
+          <FlatList
+                data={this.state.search === "" ?  this.state.allStories: this.state.data}
+                renderItem={({ item }) => (
+                  <View style={styles.itemContainer}>
+                    <Text>  Title: {item.title}</Text>
+                    <Text>  Author : {item.author}</Text>
+                  </View>
+                )}
+                keyExtractor={(item, index) => index.toString()}
+                /> 
+          
+          
+          
+        </View>  
+      );      
+    }
 }
 
 
@@ -101,7 +95,7 @@ const styles = StyleSheet.create({
   },
   item: {
     backgroundColor: 'pink',
-    padding: 10,
+    padding:10,
     marginVertical: 8,
     marginHorizontal: 16,
   },
@@ -110,10 +104,10 @@ const styles = StyleSheet.create({
   },
   itemContainer: {
     height: 80,
-    width: '100%',
+    width:'100%',
     borderWidth: 2,
     borderColor: 'pink',
-    justifyContent: 'center',
+    justifyContent:'center',
     alignSelf: 'center',
   }
 });
